ece8b744745207f999c573e6d0a6054500f0e54f
Gid <mkazli733@gmail.com>
2021-09-22 18:11:18 +0300

    updated are rules

--- eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn/Infrastructure/Data/Seed/Data/BackendConfigurationSeedAreaRules.cs
+++ eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn/Infrastructure/Data/Seed/Data/BackendConfigurationSeedAreaRules.cs
@@ -32,20 +32,306 @@ namespace BackendConfiguration.Pn.Infrastructure.Data.Seed.Data
     {
         public static IEnumerable<AreaRules> AreaRulesSeed => new[]
         {
+            // Type 1
             new AreaRules
             {
                 Id = 1,
-                EformId = 142242,
+                EformId = 0, // TODO: CHANGE
                 AreaId = 1,
                 EformName = "01. Vandforbrug",
                 AreaRuleTranslations = new List<AreaRuleTranslation>
                 {
-                    new() { AreaRuleId = 1, LanguageId = 1, Name = "Water consumption" },
-                    new() { AreaRuleId = 1, LanguageId = 4, Name = "Споживання води" },
+                    new() { AreaRuleId = 1, LanguageId = 1, Name = "Water consumption" }
                 },
                 PlanningId = null,
                 FolderId = 5,
-            }
+            },
+            new AreaRules
+            {
+                Id = 2,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 1,
+                EformName = "01. Elforbrug",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 2, LanguageId = 1, Name = "Electricity consumption" }
+                },
+                PlanningId = null,
+                FolderId = 5,
+            },
+            new AreaRules
+            {
+                Id = 3,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 2,
+                EformName = "02. Brandustyr",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 3, LanguageId = 1, Name = "Fire equipment" }
+                },
+                PlanningId = null,
+                FolderId = 7,
+            },
+            new AreaRules
+            {
+                Id = 4,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 2,
+                EformName = "02. Sikkerhedsudstyr/værnemidler",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 4, LanguageId = 1, Name = "Safety equipment" }
+                },
+                PlanningId = null,
+                FolderId = 7,
+            },
+            new AreaRules
+            {
+                Id = 5,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 2,
+                EformName = "02. Førstehjælp",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 5, LanguageId = 1, Name = "First aid" }
+                },
+                PlanningId = null,
+                FolderId = 7,
+            },
+            new AreaRules
+            {
+                Id = 6,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 7,
+                EformName = "07. Rotter",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 6, LanguageId = 1, Name = "Rats" }
+                },
+                PlanningId = null,
+                FolderId = 32,
+            },
+            new AreaRules
+            {
+                Id = 7,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 7,
+                EformName = "07. Fluer",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 7, LanguageId = 1, Name = "Flies" }
+                },
+                PlanningId = null,
+                FolderId = 32,
+            },
+            new AreaRules
+            {
+                Id = 8,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 8,
+                EformName = "08. Luftrensning timer",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 8, LanguageId = 1, Name = "Flies" }
+                },
+                PlanningId = null,
+                FolderId = 33,
+            },
+            new AreaRules
+            {
+                Id = 9,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 8,
+                EformName = "08. Luftrensning serviceaftale",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 9, LanguageId = 1, Name = "Air cleaning service agreement" }
+                },
+                PlanningId = null,
+                FolderId = 35,
+            },
+            new AreaRules
+            {
+                Id = 10,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 8,
+                EformName = "08. Luftrensning driftsstop",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 10, LanguageId = 1, Name = "Air cleaning downtime" }
+                },
+                PlanningId = null,
+                FolderId = 35,
+            },
+            new AreaRules
+            {
+                Id = 11,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 9,
+                EformName = "09. Forsuring pH værdi",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 11, LanguageId = 1, Name = "Acidification pH value" }
+                },
+                PlanningId = null,
+                FolderId = 36,
+            },
+            new AreaRules
+            {
+                Id = 12,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 9,
+                EformName = "09. Forsuring serviceaftale",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 12, LanguageId = 1, Name = "Acidification service agreement" }
+                },
+                PlanningId = null,
+                FolderId = 36,
+            },
+            new AreaRules
+            {
+                Id = 13,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 9,
+                EformName = "09. Forsuring driftsstop",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 13, LanguageId = 1, Name = "Acidification downtime" }
+                },
+                PlanningId = null,
+                FolderId = 36,
+            },
+            new AreaRules
+            {
+                Id = 13,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 9,
+                EformName = "09. Forsuring driftsstop",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 13, LanguageId = 1, Name = "Acidification downtime" }
+                },
+                PlanningId = null,
+                FolderId = 34,
+            },
+            new AreaRules
+            {
+                Id = 14,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 10,
+                EformName = "10. Varmepumpe timer og energi",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 14, LanguageId = 1, Name = "Heat pumps hours and energy" }
+                },
+                PlanningId = null,
+                FolderId = 35,
+            },
+            new AreaRules
+            {
+                Id = 15,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 10,
+                EformName = "10. Varmepumpe serviceaftale",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 15, LanguageId = 1, Name = "Heat pump service agreement" }
+                },
+                PlanningId = null,
+                FolderId = 37,
+            },
+            new AreaRules
+            {
+                Id = 16,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 10,
+                EformName = "10. Varmepumpe driftsstop",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 16, LanguageId = 1, Name = "Heat pump downtime" }
+                },
+                PlanningId = null,
+                FolderId = 37,
+            },
+            new AreaRules
+            {
+                Id = 17,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 11,
+                EformName = "11. Pillefyr",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 17, LanguageId = 1, Name = "Pellet stove" }
+                },
+                PlanningId = null,
+                FolderId = 37,
+            },
+            new AreaRules
+            {
+                Id = 18,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 12,
+                EformName = "12. Dieseltank",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 18, LanguageId = 1, Name = "Diesel tank" }
+                },
+                PlanningId = null,
+                FolderId = 38,
+            },
+            new AreaRules
+            {
+                Id = 19,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 12,
+                EformName = "12. Motor- og spildolie",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 19, LanguageId = 1, Name = "Motor oil and waste oil" }
+                },
+                PlanningId = null,
+                FolderId = 38,
+            },
+            new AreaRules
+            {
+                Id = 20,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 12,
+                EformName = "12. Kemi",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 20, LanguageId = 1, Name = "Chemistry" }
+                },
+                PlanningId = null,
+                FolderId = 38,
+            },
+            new AreaRules
+            {
+                Id = 20,
+                EformId = 0, // TODO: CHANGE
+                AreaId = 12,
+                EformName = "12. Affald og farligt affald",
+                AreaRuleTranslations = new List<AreaRuleTranslation>
+                {
+                    new() { AreaRuleId = 20, LanguageId = 1, Name = "Waste and hazardous waste" }
+                },
+                PlanningId = null,
+                FolderId = 38,
+            },
+            //new AreaRules
+            //{
+            //    Id = 21,
+            //    EformId = 0, // TODO: CHANGE
+            //    AreaId = 13,
+            //    EformName = "13. APV Medarbejder",
+            //    AreaRuleTranslations = new List<AreaRuleTranslation>
+            //    {
+            //        new() { AreaRuleId = 21, LanguageId = 1, Name = "WPA Worker" }
+            //    },
+            //    PlanningId = null,
+            //    FolderId = 39,
+            //},
         };
     }
 }
