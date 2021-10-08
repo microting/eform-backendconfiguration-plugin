79f9bb8ca8337837590622199a4c795d3cf269c8
Gid <mkazli733@gmail.com>
2021-10-08 17:49:32 +0300

    Updates for type 6

--- eform-client/src/app/plugins/modules/backend-configuration-pn/models/rule-planning/index.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/models/rule-planning/index.ts
@@ -3,3 +3,4 @@ export * from './area-rule-t1-planning.model';
 export * from './area-rule-t2-planning.model';
 export * from './area-rule-t4-planning.model';
 export * from './area-rule-t5-planning.model';
+export * from './area-rule-t6-planning.model';
