49a64b3919965a142806abf54b99619f00350d93
Alex <sahok6264@gmail.com>
2021-10-20 19:30:06 +0300

    Some change tests

--- eform-client/e2e/Tests/backend-configuration-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/backend-configuration-settings/application-settings.plugins-page.spec.ts
@@ -25,8 +25,12 @@ describe('Application settings page - site header section', function () {
       'Items Planning plugin not found or not load'
     ).not.equal(undefined);
     expect(itemsPlanningPlugin.id, 'id is not equal').equal(2);
-    expect(itemsPlanningPlugin.name, 'name is not equal').equal('Microting Backend Configuration Plugin');
-    expect(itemsPlanningPlugin.version, 'version is not equal').equal('1.0.0.0');
+    expect(itemsPlanningPlugin.name, 'name is not equal').equal(
+      'Microting Backend Configuration Plugin'
+    );
+    expect(itemsPlanningPlugin.version, 'version is not equal').equal(
+      '1.0.0.0'
+    );
     expect(itemsPlanningPlugin.status, 'status is not equal').eq(false);
   });
 
@@ -38,20 +42,20 @@ describe('Application settings page - site header section', function () {
     expect(backendPlugin.id).equal(1);
     expect(backendPlugin.name).equal('Microting Items Planning Plugin');
     expect(backendPlugin.version).equal('1.0.0.0');
-    expect(
-      backendPlugin.status,
-      'backendConfigurationPlugin is not enabled'
-    ).eq(true);
+    expect(backendPlugin.status, 'itemsPlanningPlugin is not enabled').eq(true);
 
     let itemsPlanningPlugin = await pluginPage.getPluginRowObjByIndex(2);
     await itemsPlanningPlugin.enableOrDisablePlugin();
 
     itemsPlanningPlugin = await pluginPage.getPluginRowObjByIndex(2);
     expect(itemsPlanningPlugin.id).equal(2);
-    expect(itemsPlanningPlugin.name).equal('Microting Backend Configuration Plugin');
-    expect(itemsPlanningPlugin.version).equal('1.0.0.0');
-    expect(itemsPlanningPlugin.status, 'itemsPlanningPlugin is not enabled').eq(
-      true
+    expect(itemsPlanningPlugin.name).equal(
+      'Microting Backend Configuration Plugin'
     );
+    expect(itemsPlanningPlugin.version).equal('1.0.0.0');
+    expect(
+      itemsPlanningPlugin.status,
+      'backendConfigurationPlugin is not enabled'
+    ).eq(true);
   });
 });
