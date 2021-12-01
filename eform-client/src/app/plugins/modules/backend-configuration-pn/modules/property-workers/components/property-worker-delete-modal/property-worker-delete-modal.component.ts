cc1ee47f53ab25199b3c24668caf64c134853078
Alex <sahok6264@gmail.com>
2021-12-01 16:50:20 +0200

    Add delete assignments worker if worker deleted from plugin

--- eform-client/src/app/plugins/modules/backend-configuration-pn/modules/property-workers/components/property-worker-delete-modal/property-worker-delete-modal.component.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/modules/property-workers/components/property-worker-delete-modal/property-worker-delete-modal.component.ts
@@ -1,32 +1,50 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {SiteDto} from 'src/app/common/models/dto';
-import {DeviceUserService} from 'src/app/common/services/device-users';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { SiteDto } from 'src/app/common/models/dto';
+import { DeviceUserService } from 'src/app/common/services';
+import { BackendConfigurationPnPropertiesService } from '../../../../services';
 
 @Component({
   selector: 'app-property-worker-delete-modal',
   templateUrl: './property-worker-delete-modal.component.html',
-  styleUrls: ['./property-worker-delete-modal.component.scss']
+  styleUrls: ['./property-worker-delete-modal.component.scss'],
 })
 export class PropertyWorkerDeleteModalComponent implements OnInit {
   @Input() selectedDeviceUser: SiteDto = new SiteDto();
   @Output() onUserDeleted: EventEmitter<void> = new EventEmitter<void>();
   @ViewChild('frame', { static: true }) frame;
 
-  constructor(private deviceUserService: DeviceUserService) { }
+  constructor(
+    private deviceUserService: DeviceUserService,
+    private backendConfigurationPnPropertiesService: BackendConfigurationPnPropertiesService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   show() {
     this.frame.show();
   }
 
   deleteSingle() {
-    this.deviceUserService.deleteSingleDeviceUser(this.selectedDeviceUser.siteUid).subscribe(operation => {
-      if (operation && operation.success) {
-        this.onUserDeleted.emit();
-        this.frame.hide();
-      }
-    });
+    this.deviceUserService
+      .deleteSingleDeviceUser(this.selectedDeviceUser.siteUid) // remove user from app
+      .subscribe((operation) => {
+        if (operation && operation.success) {
+          this.backendConfigurationPnPropertiesService
+            .removeWorkerAssignments(this.selectedDeviceUser.siteId) // remove user from plugin
+            .subscribe((data) => {
+              if (data && data.success) {
+                this.onUserDeleted.emit();
+                this.frame.hide();
+              }
+            });
+        }
+      });
   }
 }
