fbbf21d6c4a134b5317058c471e9be8bd1612672
Alex <sahok6264@gmail.com>
2021-12-06 17:13:06 +0200

    Add headers in table

--- eform-client/src/app/plugins/modules/backend-configuration-pn/store-providers.config.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/store-providers.config.ts
@@ -1,7 +1,9 @@
 import { propertiesPersistProvider } from './components/properties/store';
 import { propertyWorkersPersistProvider } from './modules/property-workers/components/store';
+import { compliancesPersistProvider } from './modules/compliance/components/store';
 
 export const backendConfigurationStoreProviders = [
   propertiesPersistProvider,
   propertyWorkersPersistProvider,
+  compliancesPersistProvider,
 ];
