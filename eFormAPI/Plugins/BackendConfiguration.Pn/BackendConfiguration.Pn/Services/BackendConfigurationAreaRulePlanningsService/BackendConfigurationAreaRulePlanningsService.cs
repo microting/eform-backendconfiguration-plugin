fc4745bc66ca7e01766c56a905811ac3bd969d7b
René Schultz Madsen <rm@microting.com>
2022-02-10 17:27:34 +0100

    Removing the code that deleted collected results, which we do not want. If the user wants to remove already collected cases in a given area, they should remove the individual cases.

--- eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn/Services/BackendConfigurationAreaRulePlanningsService/BackendConfigurationAreaRulePlanningsService.cs
+++ eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn/Services/BackendConfigurationAreaRulePlanningsService/BackendConfigurationAreaRulePlanningsService.cs
@@ -1240,31 +1240,6 @@ namespace BackendConfiguration.Pn.Services.BackendConfigurationAreaRulePlannings
 
                 var core = await _coreHelper.GetCore();
                 await using var sdkDbContext = core.DbContextHelper.GetDbContext();
-                var planningCases = await _itemsPlanningPnDbContext.PlanningCases
-                    .Where(x => x.WorkflowState != Constants.WorkflowStates.Removed)
-                    .Where(x => x.PlanningId == planning.Id)
-                    .ToListAsync();
-
-                foreach (var planningCase in planningCases)
-                {
-                    var planningCaseSites = await _itemsPlanningPnDbContext.PlanningCaseSites
-                        .Where(x => x.PlanningCaseId == planningCase.Id)
-                        .Where(planningCaseSite => planningCaseSite.MicrotingSdkCaseId != 0)
-                        .Where(x => x.WorkflowState != Constants.WorkflowStates.Removed)
-                        .ToListAsync();
-                    foreach (var planningCaseSite in planningCaseSites)
-                    {
-                        var result =
-                            await sdkDbContext.Cases.SingleAsync(x => x.Id == planningCaseSite.MicrotingSdkCaseId);
-                        if (result.MicrotingUid != null)
-                        {
-                            await core.CaseDelete((int)result.MicrotingUid);
-                        }
-                    }
-
-                    // Delete planning case
-                    await planningCase.Delete(_itemsPlanningPnDbContext);
-                }
 
                 var nameTranslationsPlanning =
                     planning.NameTranslations
