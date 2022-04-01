ae19e088398062f3af94417c118799af7501adc0
Alex <sahok6264@gmail.com>
2022-04-01 20:41:35 +0300

    Change initial fields for type 3(delete eform)

--- eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn/Infrastructure/Data/Seed/Data/BackendConfigurationSeedAreas.cs
+++ eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn/Infrastructure/Data/Seed/Data/BackendConfigurationSeedAreas.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 The MIT License (MIT)
 
 Copyright (c) 2007 - 2021 Microting A/S
@@ -227,7 +227,7 @@ namespace BackendConfiguration.Pn.Infrastructure.Data.Seed.Data
                 },
                 AreaInitialField = new AreaInitialField
                 {
-                    EformName = "05. Halebid og risikovurdering",
+                    // EformName = "05. Halebid og risikovurdering",
                     Notifications = true,
                     ComplianceEnabled = true,
                     RepeatEvery = 0,
