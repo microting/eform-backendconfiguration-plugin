a55cdfd2138f3b953a1d89a89269bb73ae969339
René Schultz Madsen <rm@microting.com>
2022-10-16 10:03:59 +0200

    Adding more translations.

--- eform-client/src/app/plugins/modules/backend-configuration-pn/i18n/translates.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/i18n/translates.ts
@@ -1,9 +1,11 @@
 import { enUS } from './en-US';
 import { da } from './da';
 import { deDE } from './de-DE';
+import { ukUA } from './uk-UA';
 
 export const translates = {
   'en-US': enUS,
   da: da,
   'de-DE': deDE,
+  'uk-UA': ukUA,
 };
