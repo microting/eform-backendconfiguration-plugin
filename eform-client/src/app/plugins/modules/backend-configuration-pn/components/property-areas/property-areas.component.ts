fd42e563644a40a80026f57ca8a81dc4dab2c91d
Alex <sahok6264@gmail.com>
2022-10-26 22:22:35 +0300

    Migrate property areas page to material

--- eform-client/src/app/plugins/modules/backend-configuration-pn/components/property-areas/property-areas.component.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/components/property-areas/property-areas.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {Component, OnDestroy, OnInit,} from '@angular/core';
 import {
   PropertyAreaModel,
   PropertyAreasUpdateModel,
   PropertyModel,
 } from '../../models';
-import { Subscription } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { BackendConfigurationPnPropertiesService } from '../../services';
-import { TranslateService } from '@ngx-translate/core';
-import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
-import { PropertyAreasEditModalComponent } from 'src/app/plugins/modules/backend-configuration-pn/components';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BackendConfigurationPnPropertiesService} from '../../services';
+import {TranslateService} from '@ngx-translate/core';
+import {AutoUnsubscribe} from 'ngx-auto-unsubscribe';
+import {PropertyAreasEditModalComponent} from '../../components';
 import {AuthStateService} from 'src/app/common/store';
+import {MtxGridColumn} from '@ng-matero/extensions/grid';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {Overlay} from '@angular/cdk/overlay';
+import {dialogConfigHelper} from 'src/app/common/helpers';
 
 @AutoUnsubscribe()
 @Component({
@@ -19,16 +23,14 @@ import {AuthStateService} from 'src/app/common/store';
   styleUrls: ['./property-areas.component.scss'],
 })
 export class PropertyAreasComponent implements OnInit, OnDestroy {
-  @ViewChild('editPropertyAreasModal', { static: false })
-  editPropertyAreasModal: PropertyAreasEditModalComponent;
   selectedProperty: PropertyModel = new PropertyModel();
-  selectedPropertyAreas: PropertyAreaModel[] = [];
+  selectedPropertyAreas: Array<PropertyAreaModel> = new Array<PropertyAreaModel>();
   breadcrumbs = [
     {
       name: '',
       href: '/plugins/backend-configuration-pn/properties',
     },
-    { name: '' },
+    {name: ''},
   ];
   disabledAreas: string[] = [
     '13. APV Landbrug',
@@ -37,21 +39,59 @@ export class PropertyAreasComponent implements OnInit, OnDestroy {
     '100. Diverse',
     '25. Kemisk APV',
     '00. Aflæsninger, målinger, forbrug og fækale uheld'
-  ]
+  ];
+
+  tableHeaders: MtxGridColumn[] = [
+    {
+      header: this.translateService.stream('Control area'),
+      field: 'name',
+    },
+    {
+      header: this.translateService.stream('Description'),
+      field: 'description',
+      formatter: (rowData: PropertyAreaModel) =>
+        rowData.description ? `<a href="${rowData.description}" target="_blank">${this.translateService.instant('Read more')}</a>` : ``,
+    },
+    {
+      header: this.translateService.stream('Status'),
+      field: 'status',
+      formatter: (rowData: PropertyAreaModel) => this.translateService.instant(this.getAreaPlanningStatus(rowData)),
+    },
+    {
+      header: '',
+      field: 'book',
+      type: 'button',
+      buttons: [
+        {
+          type: 'icon',
+          color: 'accent',
+          icon: 'menu_book',
+          click: (rowData: PropertyAreaModel) => this.router
+            .navigate(['../../area-rules/', rowData.id, this.selectedProperty.id], {relativeTo: this.route}),
+          tooltip: this.translateService.stream('Open area planning'),
+        }
+      ]
+    },
+  ];
 
   getTranslateSub$: Subscription;
   routerSub$: Subscription;
   getAllPropertiesDictionarySub$: Subscription;
   getPropertyAreasSub$: Subscription;
   updatePropertyAreasSub$: Subscription;
+  onUpdatePropertyAreasSub$: Subscription;
 
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog,
+    private overlay: Overlay,
+    private translateService: TranslateService,
     public authStateService: AuthStateService,
     private propertiesService: BackendConfigurationPnPropertiesService,
-    private route: ActivatedRoute,
     private backendConfigurationPnPropertiesService: BackendConfigurationPnPropertiesService,
-    private translateService: TranslateService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.routerSub$ = this.route.params.subscribe((params) => {
@@ -67,38 +107,35 @@ export class PropertyAreasComponent implements OnInit, OnDestroy {
     return area.status ? 'ON' : 'OFF';
   }
 
-  show(model: PropertyModel, propertyAreas: PropertyAreaModel[]) {
-    this.selectedPropertyAreas = [...propertyAreas];
-  }
-
-  private getProperty(selectedPropertyId: number) {
+  getProperty(selectedPropertyId: number) {
     this.getAllPropertiesDictionarySub$ = this.backendConfigurationPnPropertiesService
       .readProperty(selectedPropertyId)
       .subscribe((data) => {
         if (data && data.success) {
           this.selectedProperty = data.model;
-          this.breadcrumbs[1] = { name: this.selectedProperty.name };
+          this.breadcrumbs[1] = {name: this.selectedProperty.name};
           this.getPropertyAreas(selectedPropertyId);
         }
       });
   }
 
-  private getPropertyAreas(selectedPropertyId: number) {
+  getPropertyAreas(selectedPropertyId: number) {
     this.getPropertyAreasSub$ = this.propertiesService
       .getPropertyAreas(selectedPropertyId)
       .subscribe((data) => {
         if (data && data.success) {
-          this.selectedPropertyAreas = data.model;
+          this.selectedPropertyAreas = data.model
+            .filter(x => (!this.disabledAreas.includes(x.name) || this.authStateService.isAdmin) && x.activated);
         }
       });
   }
 
-  onUpdatePropertyAreas(model: PropertyAreasUpdateModel) {
+  onUpdatePropertyAreas(model: PropertyAreasUpdateModel, modal: MatDialogRef<PropertyAreasEditModalComponent>) {
     this.updatePropertyAreasSub$ = this.propertiesService
       .updatePropertyAreas(model)
       .subscribe((data) => {
         if (data && data.success) {
-          this.editPropertyAreasModal.hide();
+          modal.close();
           this.getPropertyAreas(this.selectedProperty.id);
         }
       });
@@ -108,9 +145,13 @@ export class PropertyAreasComponent implements OnInit, OnDestroy {
     this.getPropertyAreasSub$ = this.propertiesService
       .getPropertyAreas(this.selectedProperty.id)
       .subscribe((data) => {
-        this.editPropertyAreasModal.show(this.selectedProperty, data.model);
+        const modal = this.dialog.open(PropertyAreasEditModalComponent,
+          {...dialogConfigHelper(this.overlay, {selectedProperty: this.selectedProperty, propertyAreas: data.model}), minWidth: 400});
+        this.onUpdatePropertyAreasSub$ = modal.componentInstance.updatePropertyAreas
+          .subscribe(x => this.onUpdatePropertyAreas(x, modal));
       });
   }
 
-  ngOnDestroy() {}
+  ngOnDestroy() {
+  }
 }
