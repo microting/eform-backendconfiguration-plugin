4857427ea042bfe983874499140f3a9afa8b6a29
René Schultz Madsen <rm@microting.com>
2022-12-06 19:01:22 +0100

    Fixing setting task management as on by default.

--- eform-client/src/app/plugins/modules/backend-configuration-pn/modules/property-workers/components/property-worker-create-edit-modal/property-worker-create-edit-modal.component.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/modules/property-workers/components/property-worker-create-edit-modal/property-worker-create-edit-modal.component.ts
@@ -76,7 +76,10 @@ export class PropertyWorkerCreateEditModalComponent implements OnInit, OnDestroy
       this.edit = true;
     }
     if (!this.edit) {
-      this.selectedDeviceUser.languageCode = this.languages[1].locale;
+      this.selectedDeviceUser.languageCode = this.languages[1].locale; // set to 1 due to current tests.
+      if (this.authStateService.checkClaim('task_management_enable')) {
+        this.selectedDeviceUser.taskManagementEnabled = true;
+      }
     }
   }
 
