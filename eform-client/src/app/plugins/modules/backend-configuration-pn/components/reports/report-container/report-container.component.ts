8803b8c58b2b8974061f807de6500890aa0ba417
René Schultz Madsen <rm@microting.com>
2023-04-24 08:36:39 +0000

    Fixing the broken date picker.

--- eform-client/src/app/plugins/modules/backend-configuration-pn/components/reports/report-container/report-container.component.ts
+++ eform-client/src/app/plugins/modules/backend-configuration-pn/components/reports/report-container/report-container.component.ts
@@ -108,8 +108,8 @@ export class ReportContainerComponent implements OnInit, OnDestroy {
   }
 
   onGenerateReport(model: ReportPnGenerateModel) {
-    this.dateFrom = format(parseISO(model.dateFrom), `yyyy-MM-dd'T'HH:mm:ss`);
-    this.dateTo = format(parseISO(model.dateTo), `yyyy-MM-dd'T'HH:mm:ss`);
+    this.dateFrom = model.dateFrom;
+    this.dateTo = model.dateTo;
     this.generateReportSub$ = this.reportService
       .generateReport({
         dateFrom: model.dateFrom,
