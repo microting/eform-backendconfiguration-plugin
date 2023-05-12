715ab6edb7b224140d76b70d2607898b79af6c14
René Schultz Madsen <rm@microting.com>
2023-05-12 12:21:43 +0200

    Updating to use the new testcontainer.

--- eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn.Integration.Test/TestBaseSetup.cs
+++ eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn.Integration.Test/TestBaseSetup.cs
@@ -10,22 +10,19 @@ using Microting.eFormCaseTemplateBase.Infrastructure.Data;
 using Microting.ItemsPlanningBase.Infrastructure.Data;
 using Microting.TimePlanningBase.Infrastructure.Data;
 using Rebus.Bus;
+using Testcontainers.MariaDb;
 
 namespace BackendConfiguration.Pn.Integration.Test;
 
 public class TestBaseSetup
 {
-    #pragma warning disable CS0618
-    private readonly MariaDbTestcontainer _mySqlTestcontainer = new ContainerBuilder<MariaDbTestcontainer>()
-#pragma warning restore CS0618
-        .WithDatabase(new MySqlTestcontainerConfiguration(image: "mariadb:latest")
-        {
-            Database = "myDb",
-            Username = "root",
-            Password = "Qq1234567$"
-        })
+    private readonly MariaDbContainer _mariadbTestcontainer = new MariaDbBuilder()
+        .WithDatabase(
+            "myDb").WithUsername("bla").WithPassword("secretpassword")
         .WithEnvironment("MYSQL_ROOT_PASSWORD", "Qq1234567$")
         .Build();
+
+    protected MicrotingDbContext DbContext;
 
     protected BackendConfigurationPnDbContext? BackendConfigurationPnDbContext;
     protected ItemsPlanningPnDbContext? ItemsPlanningPnDbContext;
@@ -38,7 +35,7 @@ public class TestBaseSetup
     {
         var optionsBuilder = new DbContextOptionsBuilder<BackendConfigurationPnDbContext>();
 
-        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-backend-configuration-plugin"), new MariaDbServerVersion(
+        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-backend-configuration-plugin").Replace("bla", "root"), new MariaDbServerVersion(
             new Version(10, 8)));
 
         var backendConfigurationPnDbContext = new BackendConfigurationPnDbContext(optionsBuilder.Options);
@@ -61,7 +58,7 @@ public class TestBaseSetup
     {
         var optionsBuilder = new DbContextOptionsBuilder<ItemsPlanningPnDbContext>();
 
-        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-angular-items-planning-plugin"), new MariaDbServerVersion(
+        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-angular-items-planning-plugin").Replace("bla", "root"), new MariaDbServerVersion(
             new Version(10, 8)));
 
         var backendConfigurationPnDbContext = new ItemsPlanningPnDbContext(optionsBuilder.Options);
@@ -78,7 +75,7 @@ public class TestBaseSetup
     {
         var optionsBuilder = new DbContextOptionsBuilder<TimePlanningPnDbContext>();
 
-        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-angular-items-planning-plugin"), new MariaDbServerVersion(
+        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-angular-items-planning-plugin").Replace("bla", "root"), new MariaDbServerVersion(
             new Version(10, 8)));
 
         var backendConfigurationPnDbContext = new TimePlanningPnDbContext(optionsBuilder.Options);
@@ -95,7 +92,7 @@ public class TestBaseSetup
     {
         var optionsBuilder = new DbContextOptionsBuilder<CaseTemplatePnDbContext>();
 
-        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-angular-case-template-plugin"), new MariaDbServerVersion(
+        optionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_eform-angular-case-template-plugin").Replace("bla", "root"), new MariaDbServerVersion(
             new Version(10, 8)));
 
         var backendConfigurationPnDbContext = new CaseTemplatePnDbContext(optionsBuilder.Options);
@@ -112,7 +109,7 @@ public class TestBaseSetup
     {
         DbContextOptionsBuilder dbContextOptionsBuilder = new DbContextOptionsBuilder();
 
-        dbContextOptionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_SDK"), new MariaDbServerVersion(
+        dbContextOptionsBuilder.UseMySql(connectionStr.Replace("myDb", "420_SDK").Replace("bla", "root"), new MariaDbServerVersion(
             new Version(10, 8)));
         var microtingDbContext =  new MicrotingDbContext(dbContextOptionsBuilder.Options);
         string file = Path.Combine("SQL", "420_SDK.sql");
@@ -127,7 +124,7 @@ public class TestBaseSetup
     protected async Task<Core> GetCore()
     {
         var core = new Core();
-        await core.StartSqlOnly(_mySqlTestcontainer.ConnectionString.Replace("myDb", "420_SDK"));
+        await core.StartSqlOnly(_mariadbTestcontainer.GetConnectionString().Replace("myDb", "420_SDK").Replace("bla", "root"));
         return core;
     }
 
@@ -135,32 +132,32 @@ public class TestBaseSetup
     public async Task Setup()
     {
         Console.WriteLine($"{DateTime.Now} : Starting MariaDb Container...");
-        await _mySqlTestcontainer.StartAsync();
+        await _mariadbTestcontainer.StartAsync();
         Console.WriteLine($"{DateTime.Now} : Started MariaDb Container");
 
-        BackendConfigurationPnDbContext = GetBackendDbContext(_mySqlTestcontainer.ConnectionString);
+        BackendConfigurationPnDbContext = GetBackendDbContext(_mariadbTestcontainer.GetConnectionString());
 
         BackendConfigurationPnDbContext!.Database.SetCommandTimeout(300);
 
-        ItemsPlanningPnDbContext = GetItemsPlanningPnDbContext(_mySqlTestcontainer.ConnectionString);
+        ItemsPlanningPnDbContext = GetItemsPlanningPnDbContext(_mariadbTestcontainer.GetConnectionString());
 
         ItemsPlanningPnDbContext.Database.SetCommandTimeout(300);
 
-        TimePlanningPnDbContext = GetTimePlanningPnDbContext(_mySqlTestcontainer.ConnectionString);
+        TimePlanningPnDbContext = GetTimePlanningPnDbContext(_mariadbTestcontainer.GetConnectionString());
 
         TimePlanningPnDbContext.Database.SetCommandTimeout(300);
 
-        MicrotingDbContext = GetContext(_mySqlTestcontainer.ConnectionString);
+        MicrotingDbContext = GetContext(_mariadbTestcontainer.GetConnectionString());
 
         MicrotingDbContext.Database.SetCommandTimeout(300);
 
-        CaseTemplatePnDbContext = GetCaseTemplatePnDbContext(_mySqlTestcontainer.ConnectionString);
+        CaseTemplatePnDbContext = GetCaseTemplatePnDbContext(_mariadbTestcontainer.GetConnectionString());
 
         CaseTemplatePnDbContext.Database.SetCommandTimeout(300);
 
         var rebusService =
-            new RebusService(new EFormCoreService(_mySqlTestcontainer.ConnectionString.Replace("myDb", "420_SDK")), new BackendConfigurationLocalizationService());
-        rebusService.Start(_mySqlTestcontainer.ConnectionString.Replace("myDb", "420_SDK")).GetAwaiter().GetResult();
+            new RebusService(new EFormCoreService(_mariadbTestcontainer.GetConnectionString().Replace("myDb", "420_SDK").Replace("bla", "root")), new BackendConfigurationLocalizationService());
+        rebusService.Start(_mariadbTestcontainer.GetConnectionString().Replace("myDb", "420_SDK").Replace("bla", "root")).GetAwaiter().GetResult();
         Bus = rebusService.GetBus();
     }
 }
