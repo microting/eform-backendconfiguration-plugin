7ea6d647280e809e0aa9414e0854f1e583a8a57e
René Schultz Madsen <rm@microting.dk>
2025-09-10 15:05:57 +0200

    Fixing upgradescript.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -4,9 +4,9 @@ GIT_STATUS=`git status | grep "nothing to commit, working tree clean" | wc -l`
 if (( "$GIT_STATUS" > 0 )); then
 	git checkout master
 	git pull
-	cd eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn
   CURRENT_NUMBER_OF_COMMITS=`git log --oneline | wc -l`
 
+	cd eFormAPI/Plugins/BackendConfiguration.Pn/BackendConfiguration.Pn
 	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn' 'Microting.EformBackendConfigurationBase' 'Microting.ItemsPlanningBase' 'Microting.TimePlanningBase' 'Microting.EformAngularFrontendBase' 'Microting.eFormCaseTemplateBase' 'Google.Apis.Sheets.v4' 'Sentry')
 	PROJECT_NAME='BackendConfiguration.Pn.csproj'
 	REPOSITORY='eform-angular-backendconfiguration-plugin'
@@ -42,6 +42,84 @@ if (( "$GIT_STATUS" > 0 )); then
 		  git commit -a -m "closes #$ISSUE_NUMBER"
 		fi
 	done
+
+	cd ..
+	cd BackendConfiguration.Pn.Integration.Test
+	PACKAGES=('Microsoft.NET.Test.Sdk' 'NSubstitute' 'NUnit' 'NUnit3TestAdapter' 'NUnit.Analyzers' 'Testcontainers' 'Testcontainers.Mariadb')
+	PROJECT_NAME='BackendConfiguration.Pn.Integration.Test.csproj'
+	REPOSITORY='eform-angular-backendconfiguration-plugin'
+
+	for PACKAGE_NAME in ${PACKAGES[@]}; do
+
+		OLD_VERSION=`dotnet list package | grep "$PACKAGE_NAME " | grep -oP ' \d\.\d+\.\d.*' | grep -oP ' \d.* \b' | xargs`
+
+		dotnet add $PROJECT_NAME package $PACKAGE_NAME
+
+		NEW_VERSION=`dotnet list package | grep "$PACKAGE_NAME " | grep -oP ' \d\.\d+\.\d.*$' | grep -oP '\d\.\d+\.\d.*$' | grep -oP ' \d\.\d+\.\d.*$' | xargs`
+
+		if [ $NEW_VERSION != $OLD_VERSION ]; then
+		  echo "We have a new version of $PACKAGE_NAME, so creating github issue and do a commit message to close that said issue"
+		  RESULT=`curl -X "POST" "https://api.github.com/repos/microting/$REPOSITORY/issues?state=all" \
+		     -H "Cookie: logged_in=no" \
+		     -H "Authorization: token $CHANGELOG_GITHUB_TOKEN" \
+		     -H "Content-Type: text/plain; charset=utf-8" \
+		     -d $'{
+		  "title": "Bump '$PACKAGE_NAME' from '$OLD_VERSION' to '$NEW_VERSION'",
+		  "body": "TBD",
+		  "assignees": [
+		    "renemadsen"
+		  ],
+		  "labels": [
+		    ".NET",
+		    "backend",
+		    "enhancement"
+		  ]
+		}'`
+			ISSUE_NUMBER=`echo $RESULT | grep -oP 'number": \d+,' | grep -oP '\d+'`
+		  git add .
+		  git commit -a -m "closes #$ISSUE_NUMBER"
+		fi
+	done
+
+
+	cd ..
+	cd BackendConfiguration.Pn.Test
+	PACKAGES=('Microsoft.NET.Test.Sdk' 'NSubstitute' 'NUnit' 'NUnit3TestAdapter' 'NUnit.Analyzers' 'Testcontainers' 'Testcontainers.Mariadb')
+	PROJECT_NAME='BackendConfiguration.Pn.Test.csproj'
+	REPOSITORY='eform-angular-backendconfiguration-plugin'
+
+	for PACKAGE_NAME in ${PACKAGES[@]}; do
+
+		OLD_VERSION=`dotnet list package | grep "$PACKAGE_NAME " | grep -oP ' \d\.\d+\.\d.*' | grep -oP ' \d.* \b' | xargs`
+
+		dotnet add $PROJECT_NAME package $PACKAGE_NAME
+
+		NEW_VERSION=`dotnet list package | grep "$PACKAGE_NAME " | grep -oP ' \d\.\d+\.\d.*$' | grep -oP '\d\.\d+\.\d.*$' | grep -oP ' \d\.\d+\.\d.*$' | xargs`
+
+		if [ $NEW_VERSION != $OLD_VERSION ]; then
+		  echo "We have a new version of $PACKAGE_NAME, so creating github issue and do a commit message to close that said issue"
+		  RESULT=`curl -X "POST" "https://api.github.com/repos/microting/$REPOSITORY/issues?state=all" \
+		     -H "Cookie: logged_in=no" \
+		     -H "Authorization: token $CHANGELOG_GITHUB_TOKEN" \
+		     -H "Content-Type: text/plain; charset=utf-8" \
+		     -d $'{
+		  "title": "Bump '$PACKAGE_NAME' from '$OLD_VERSION' to '$NEW_VERSION'",
+		  "body": "TBD",
+		  "assignees": [
+		    "renemadsen"
+		  ],
+		  "labels": [
+		    ".NET",
+		    "backend",
+		    "enhancement"
+		  ]
+		}'`
+			ISSUE_NUMBER=`echo $RESULT | grep -oP 'number": \d+,' | grep -oP '\d+'`
+		  git add .
+		  git commit -a -m "closes #$ISSUE_NUMBER"
+		fi
+	done
+
 	NEW_NUMBER_OF_COMMITS=`git log --oneline | wc -l`
 
 	if (( $NEW_NUMBER_OF_COMMITS > $CURRENT_NUMBER_OF_COMMITS )); then
